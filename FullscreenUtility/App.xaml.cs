7e236521928921eaf9d326765a0c653e58545b97
Morgan Creekmore <morgan@creekmore.email>
2021-08-17 19:37:40 -0500

    Add skeleton

---
+++ FullscreenUtility/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace FullscreenUtility
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private readonly TaskbarIcon _trayIcon;
+
+        public App()
+        {
+            InitializeComponent();
+
+            _trayIcon = (TaskbarIcon)FindResource("TrayIcon");
+        }
+
+        private async void OnStartup(object sender, StartupEventArgs e)
+        {
+        }
+
+        private async void OnExit(object sender, ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
+
+        private void ExitOnClick(object sender, RoutedEventArgs e)
+        {
+            Current.Shutdown();
+        }
+
+        private void ToggleCursorLock(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ToggleMouseTransparency(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
